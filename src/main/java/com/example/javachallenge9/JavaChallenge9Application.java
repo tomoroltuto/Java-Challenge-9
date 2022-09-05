93a551994fa9153cafcce021b72853972dfa7150
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-09-05 22:00:26 +0900

    first commit

---
+++ src/main/java/com/example/javachallenge9/JavaChallenge9Application.java
@@ -0,0 +1,13 @@
+package com.example.javachallenge9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallenge9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallenge9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javachallenge9/JavaChallenge9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javachallenge9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallenge9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
